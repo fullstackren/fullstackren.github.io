11de2b5f7c6d04d475c053538fce7aa30eb405e1
fullstackren <liferzy@163.com>
2024-12-29 23:37:08 +0800

    feat:开发者路线

--- docs/components/df-roadmap/df-roadmap-data.ts
+++ docs/components/df-roadmap/df-roadmap-data.ts
@@ -11,11 +11,55 @@ export const ROADMAP_DATA: NavData[] = [
     title: '高质量博客',
     items: [
       {
+        title: 'Fresh Frontend Links',
+        desc: '新鲜的前端链接。',
+        badge: "每周更新",
+        link: 'https://frontender-ua.medium.com/',
+        icon: '/icons/medium.png',
+      },
+      {
         title: '科技爱好者周刊',
         desc: '阮一峰的科技爱好者周刊。',
         badge: "每周五更新",
         link: 'https://www.ruanyifeng.com/blog/archives.html',
         icon: '/icons/ruanyifeng.jpeg',
+      },
+      {
+        title: '冴羽的博客',
+        desc: 'github issue维护的博客文章。',
+        link: 'https://github.com/mqyqingfeng/Blog',
+        icon: 'https://avatars.githubusercontent.com/u/11458263?s=64&v=4',
+      },
+      {
+        title: '前端精读',
+        desc: 'github issue维护的博客文章。',
+        link: 'https://github.com/ascoders/weekly',
+        icon: 'https://avatars.githubusercontent.com/u/7970947?s=48&v=4',
+      },
+      {
+        title: '小林coding',
+        desc: '小林 x 图解计算机基础（计算机网络、操作系统、计算机组成、数据库）。',
+        link: 'https://xiaolincoding.com/',
+        icon: 'https://xiaolincoding.com/logo.webp',
+      },
+      
+      {
+        title: 'antfu.me',
+        desc: 'Vue, Vite, Nuxt 核心团队成员 | VueUse, Slidev, Vitest, UnoCSS 作者。',
+        link: 'https://antfu.me/posts',
+        icon: 'https://notes.fe-mm.com/icons/antfu.svg',
+      },
+      {
+        title: '鑫空间-鑫生活',
+        desc: '张鑫旭的博客。',
+        link: 'https://www.zhangxinxu.com/',
+        icon: 'https://cdn.zhangxinxu.com/wordpress/wp-content/themes/default/images/index_logo.gif',
+      },
+      {
+        title: '奇舞周刊',
+        desc: '领略前端技术 阅读奇舞周刊（360 前端团队）。',
+        link: 'https://weekly.75.team/',
+        icon: 'https://s0.ssl.qhres2.com/static/3444fd94a3c31e92.ico',
       },
     ]
   },
@@ -105,18 +149,6 @@ export const ROADMAP_DATA: NavData[] = [
         desc: '插件化的企业级前端应用框架',
         link: 'https://umijs.org/',
         icon: '/icons/umi.png',
-      },
-      {
-        title: '',
-        desc: '',
-        link: '',
-        icon: '',
-      },
-      {
-        title: '',
-        desc: '',
-        link: '',
-        icon: '',
       },
     ]
   },
@@ -240,6 +272,47 @@ export const ROADMAP_DATA: NavData[] = [
         link: 'https://lerna.js.org/',
         icon: '/icons/lerna.svg'
       }
+    ]
+  },
+  {
+    title: 'Node.js & Deno',
+    items: [
+      {
+        title: 'Node.js',
+        desc: '基于 V8 引擎的 JavaScript 运行环境',
+        link: 'https://nodejs.cn/',
+        icon: '/icons/node.svg',
+      },
+      {
+        title: 'Deno',
+        desc: 'Deno 是一个 JavaScript/TypeScript 的运行时',
+        link: 'https://manual.deno.js.cn/introduction',
+        icon: '/icons/deno.svg',
+      },
+      {
+        title: 'Koa',
+        desc: '',
+        link: 'https://koajs.com/',
+        icon: '/icons/koa.svg',
+      },
+      {
+        title: '',
+        desc: '',
+        link: '',
+        icon: '',
+      },
+      {
+        title: '',
+        desc: '',
+        link: '',
+        icon: '',
+      },
+      {
+        title: '',
+        desc: '',
+        link: '',
+        icon: '',
+      },
     ]
   },
   {
