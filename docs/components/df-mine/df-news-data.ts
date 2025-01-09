7b7aee15e3333e8d5e02b69b8c85e7969881a8b5
fullstackren <liferzy@163.com>
2025-01-09 14:26:43 +0800

    feat:配置

--- docs/components/df-mine/df-news-data.ts
+++ docs/components/df-mine/df-news-data.ts
@@ -5,22 +5,22 @@ export const NEWS_DATA: MineData[] = [
   {
     title: 'AI 中文',
     time: "2025-01-01",
-    desc: '探索智能边界，塑造未来无限可能，这是我搭建的<a href="http://ai.fullstack.ren/" :target="_blank" rel="noreferrer">《AI 中文》</a>',
-    image: '/course/ai.jpeg',
-    icon: '/logo.jpg',
+    desc: '简化工作流程，让开发更简单，这是我搭建的<a href="http://ai.fullstack.ren/" :target="_blank" rel="noreferrer">《AI 中文》</a>',
+    image: '/news/ai.jpeg',
+    icon: '/icons/ai.png',
   },
   {
     title: 'Chrome 中文',
     time: "2020-01-01",
     desc: '精简了官网文档，方便阅读和指导日常开发使用，这是我搭建的<a href="http://chrome.fullstack.ren/" :target="_blank" rel="noreferrer">《Chrome 中文》</a>',
-    image: '/mine/chrome.webp',
+    image: '/news/chrome.webp',
     icon: '/icons/chrome.png',
   },
   {
     title: '博客搭建',
     time: "2021-01-01",
     desc: '记录技术成长和过程。',
-    image: '/mine/screen.png',
-    icon: '/logo.jpg'
+    image: '/news/screen.png',
+    icon: '/icons/jianghu.jpg'
   }
 ]
--- docs/make-a-friend.md
+++ docs/make-a-friend.md
@@ -6,14 +6,19 @@ outline: [2, 3, 4]
 
 <script setup>
 import DfMine from './components/df-mine/DfMine.vue'
-
-import { MAKE_A_FRIEND_DATA } from './components/df-mine/df-mine-data.ts'
+import { CONTACT_DATA, OUTPUT_DATA } from './components/df-mine/df-mine-data.ts'
 </script>
 
 # 交个朋友
 
+## 联系方式
+
 <ClientOnly>
-  <div style="margin-top: 20px;">
-    <DfMine v-bind:data="MAKE_A_FRIEND_DATA" />
-  </div>
+  <DfMine v-bind:data="CONTACT_DATA" />
+</ClientOnly>
+
+## 内容输出
+
+<ClientOnly>
+  <DfMine v-bind:data="OUTPUT_DATA" />
 </ClientOnly>
