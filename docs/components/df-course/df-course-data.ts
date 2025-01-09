5c4cf7efe937c8c0a5e2e80ca987909101afa163
fullstackren <liferzy@163.com>
2025-01-09 13:25:45 +0800

    feat:ai中文

--- docs/components/df-course/df-course-data.ts
+++ docs/components/df-course/df-course-data.ts
@@ -3,7 +3,7 @@ import type { CourseData } from './types'
 // 专栏专讲-AI专栏
 export const AI_DATA: CourseData[] = [
   {
-    title: 'AI中文',
+    title: 'AI 中文',
     desc: '探索智能边界，塑造未来无限可能。',
     link: 'http://ai.fullstack.ren/',
     image: '/course/ai.jpeg',
@@ -13,7 +13,7 @@ export const AI_DATA: CourseData[] = [
 // 专栏专讲-语言专栏
 export const LANGUAGE_DATA: CourseData[] = [
   {
-    title: 'Rust中文',
+    title: 'Rust 中文',
     desc: '一种赋予每个人力量的语言构建可靠、高效的软件。',
     link: 'http://rust.fullstack.ren/',
     image: '/course/rust.png',
@@ -23,13 +23,13 @@ export const LANGUAGE_DATA: CourseData[] = [
 // 专栏专讲-开发环境专栏
 export const ENGINEERING_DATA: CourseData[] = [
   {
-    title: 'Chrome中文',
+    title: 'Chrome 中文',
     desc: '精简了官网文档，方便阅读。',
     link: 'http://chrome.fullstack.ren/',
     image: '/course/chrome.jpg',
   },
   {
-    title: 'Babel中文',
+    title: 'Babel 中文',
     desc: 'Babel 是一个 JavaScript 编译器。',
     link: 'http://babeljs.fullstack.ren/',
     image: '/icons/babel.png',
--- docs/components/df-mine/df-news-data.ts
+++ docs/components/df-mine/df-news-data.ts
@@ -3,14 +3,14 @@ import type { MineData } from './types'
 // 最新动态
 export const NEWS_DATA: MineData[] = [
   {
-    title: 'AI中文',
+    title: 'AI 中文',
     time: "2025-01-01",
     desc: '探索智能边界，塑造未来无限可能，这是我搭建的<a href="http://ai.fullstack.ren/" :target="_blank" rel="noreferrer">《AI 中文》</a>',
     image: '/course/ai.jpeg',
     icon: '/logo.jpg',
   },
   {
-    title: 'Chrome中文',
+    title: 'Chrome 中文',
     time: "2020-01-01",
     desc: '精简了官网文档，方便阅读和指导日常开发使用，这是我搭建的<a href="http://chrome.fullstack.ren/" :target="_blank" rel="noreferrer">《Chrome 中文》</a>',
     image: '/mine/chrome.webp',
