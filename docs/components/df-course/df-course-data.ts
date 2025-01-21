5a0004a6e6b250d89ce670dc8a7a0d5e6e01bb7e
fullstackren <liferzy@163.com>
2025-01-21 15:35:38 +0800

    feat:news

--- docs/components/df-course/df-course-data.ts
+++ docs/components/df-course/df-course-data.ts
@@ -4,6 +4,12 @@ import type { CourseData } from './types'
 // 专栏专讲-AI专栏
 export const AI_DATA: CourseData[] = [
   {
+    title: 'LangChain 中文',
+    desc: '自己搭建 LangChain 中文，阅读更友好和顺手',
+    link: 'http://langchain.fullstack.ren/',
+    image: '/course/langchain.png'
+  },
+  {
     title: 'AI 中文',
     desc: '探索智能边界，塑造未来无限可能。',
     link: 'http://ai.fullstack.ren/',
@@ -18,6 +24,12 @@ export const LANGUAGE_DATA: CourseData[] = [
     desc: '一种赋予每个人力量的语言构建可靠、高效的软件。',
     link: 'http://rust.fullstack.ren/',
     image: '/course/rust.png',
+  },
+  {
+    title: 'Python 中文',
+    desc: '自己搭建 Python 中文，阅读更友好和顺手',
+    link: 'http://python.fullstack.ren/',
+    image: '/news/python.jpg',
   },
 ]
 
--- docs/components/df-mine/df-news-data.ts
+++ docs/components/df-mine/df-news-data.ts
@@ -3,6 +3,20 @@ import type { MineData } from './types'
 // 最新动态
 export const NEWS_DATA: MineData[] = [
   {
+    title: 'LangChain 中文',
+    time: "2025-01-01",
+    desc: '中文网的 LangChain 使用指南对我来说阅读不是很友好和顺手，这是我搭建的<a href="http://langchain.fullstack.ren/" :target="_blank" rel="noreferrer">《LangChain 中文》</a>',
+    image: '/news/langchain.png',
+    icon: '/icons/langchain.png',
+  },
+  {
+    title: 'Python 中文',
+    time: "2025-01-01",
+    desc: '中文网的 Python 使用指南对我来说阅读不是很友好和顺手，这是我搭建的<a href="http://python.fullstack.ren/" :target="_blank" rel="noreferrer">《Python 中文》</a>',
+    image: '/news/python.jpg',
+    icon: '/icons/python.svg',
+  },
+  {
     title: 'AI 中文',
     time: "2025-01-01",
     desc: '简化工作流程，让开发更简单，这是我搭建的<a href="http://ai.fullstack.ren/" :target="_blank" rel="noreferrer">《AI 中文》</a>',
